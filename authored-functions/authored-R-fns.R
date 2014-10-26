308cfce14038dae6754f2b83b32dcfdb879f6128
tdmcarthur <travis.d.mcarthur@gmail.com>
2014-10-26 17:08:54 -0500

    edit to replace-vars-string function

--- authored-functions/authored-R-fns.R
+++ authored-functions/authored-R-fns.R
@@ -28,5 +28,23 @@ replace.vars <- function(replacement.matrix, directory, file.pattern, table.only
 #  directory="/Users/travismcarthur/Desktop/Dev - AAE 642/Problem sets/PS 3/", 
 #  file.pattern="table.*tex", table.only=TRUE)
   
- 
+
+
+
+replace.vars.string <- function(replacement.matrix, input.table, html=FALSE, table.only=TRUE ) {
+  
+    tex.text <- input.table
+    for ( j in 1:nrow(replacement.matrix)) {
+      if (replacement.matrix[j, 2]=="") {next}
+      if (!html) {
+      table.only.vec <- grep("hline", tex.text) 
+      table.only.vec <- (1:length(tex.text))[min(table.only.vec)<=1:length(tex.text)  &
+          1:length(tex.text)<=max(table.only.vec)]
+      }
+      if (!table.only) {table.only.vec <- 1:length(tex.text)}
+      tex.text[table.only.vec] <- gsub(pattern=replacement.matrix[j, 1], 
+        replacement=replacement.matrix[j, 2], x=tex.text[table.only.vec],  fixed=TRUE)
+    }
+    tex.text
+}
 
